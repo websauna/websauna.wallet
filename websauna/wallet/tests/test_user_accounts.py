665982ea0055ee69bc896aa5cf96e72eaa318483
Mikko Ohtamaa <mikko@opensourcehacker.com>
2016-07-03 19:42:53 -0700

    Update to latest WEbsauna master.

--- websauna/wallet/tests/test_user_accounts.py
+++ websauna/wallet/tests/test_user_accounts.py
@@ -1,5 +1,7 @@
 import pytest
 import transaction
+from decimal import Decimal
+
 from websauna.tests.utils import create_user
 from websauna.tests.webserver import customized_web_server
 
@@ -33,5 +35,5 @@ def test_user_account_top_up(dbsession, registry):
         oa = UserOwnedAccount.create_for_user(user=user, asset=asset)
         dbsession.flush()
 
-        oa.account.do_withdraw_or_deposit(+100, "Topping up")
+        oa.account.do_withdraw_or_deposit(Decimal("+100"), "Topping up")
 
