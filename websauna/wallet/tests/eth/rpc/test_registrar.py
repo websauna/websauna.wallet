5e597fa95d9b9b4b3ddf8ab19427d23d31158e5d
Mikko Ohtamaa <mikko@opensourcehacker.com>
2016-08-17 05:22:07 +0300

    Oh gosh it does not work.

--- websauna/wallet/tests/eth/rpc/test_registrar.py
+++ websauna/wallet/tests/eth/rpc/test_registrar.py
@@ -7,7 +7,7 @@ from web3 import Web3
 from websauna.wallet.ethereum.compiler import get_compiled_contract_cached
 
 from websauna.wallet.ethereum.contract import Contract, confirm_transaction, deploy_contract, get_contract
-from websauna.wallet.ethereum.utils import wei_to_eth, eth_address_to_bin
+from websauna.wallet.ethereum.utils import wei_to_eth, eth_address_to_bin, to_wei
 
 from websauna.wallet.tests.eth.utils import wait_tx, send_balance_to_contract
 
@@ -16,8 +16,49 @@ TEST_VALUE = Decimal("0.01")
 
 
 @pytest.mark.slow
-def xxx_test_registrar_based_wallet(web3: Web3, coinbase):
+def test_registrar_based_wallet(web3: Web3, coinbase):
     """Create registrar contract and register a wallet against it."""
+
+    wei_amount = to_wei(TEST_VALUE)
+
+    # Check we get somewhat valid ids
+    contract_def = get_compiled_contract_cached("OwnedRegistrar")
+    registrar_contract, txid = deploy_contract(web3, contract_def)
+
+    # Deploy wallet contract body
+    contract_def = get_compiled_contract_cached("Wallet")
+    wallet_contract, txid = deploy_contract(web3, contract_def)
+
+    # Register wallet contract body
+    txid = registrar_contract.transact().setAddr("wallet", wallet_contract.address)
+    confirm_transaction(web3, txid)
+
+    # Deploy relay against the registered wallet
+    contract_def = get_compiled_contract_cached("Relay")
+    relay, txid = deploy_contract(web3, contract_def, constructor_arguments=[registrar_contract.address, "wallet"])
+
+    # Test relayed wallet. We use Wallet ABI
+    # against Relay contract.
+    contract_def = get_compiled_contract_cached("Wallet")
+    relayed_wallet = get_contract(web3, contract_def, relay.address)
+    # Works
+    assert wallet_contract.call().version().decode("utf-8") == "1.0"
+    # Fails: Not enough data for head
+    assert relayed_wallet.call().version().decode("utf-8") == "1.0"
+
+    # Deposit some ETH
+    txid = send_balance_to_contract(relayed_wallet.address, wei_amount)
+    confirm_transaction(web3, txid)
+    assert relayed_wallet.web3.eth.getBalance(relayed_wallet.address, wei_amount)
+
+    # Withdraw ETH back
+    relayed_wallet.transact().withdraw(coinbase, wei_amount)
+    confirm_transaction(web3, txid)
+    assert relayed_wallet.web3.eth.getBalance(relayed_wallet.address, 0)
+
+
+@pytest.mark.slow
+def xxx_test_upgrade_wallet(web3: Web3):
 
     # Check we get somewhat valid ids
     contract_def = get_compiled_contract_cached("OwnedRegistrar")
@@ -41,15 +82,10 @@ def xxx_test_registrar_based_wallet(web3: Web3, coinbase):
     relayed_wallet = get_contract(web3, contract_def, relay.address)
     assert relayed_wallet.call().version() == "1.0"
 
-    # Deposit some ETH
-    txid = send_balance_to_contract(relayed_wallet.address, TEST_VALUE)
-    confirm_transaction(web3, txid)
-    assert relayed_wallet.web3.eth.getBalance(relayed_wallet.address, TEST_VALUE)
+    #
+    # Upgrade wallet
+    #
 
-    # Withdraw ETH back
-    relayed_wallet.transact().transfer()
-
-
-@pytest.mark.slow
-def xxx_test_upgrade_wallet(web3: Web3):
-    pass
+    # Deploy wallet contract body
+    contract_def = get_compiled_contract_cached("Wallet")
+    wallet_contract, txid = deploy_contract(web3, contract_def)
