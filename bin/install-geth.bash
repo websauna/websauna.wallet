bb9a4f73909ec95a95c580c5f4241209f630175a
Mikko Ohtamaa <mikko@opensourcehacker.com>
2016-09-08 23:32:58 +0300

    Add geth install script.

---
+++ bin/install-geth.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Install geth monolithic binary
+#
+
+if [ ! -e geth/geth ] ; then
+    wget -O geth.tar.bz2 https://github.com/ethereum/go-ethereum/releases/download/v1.4.11/geth-Linux64-20160819135300-1.4.11-fed692f.tar.bz2
+    install -d geth
+    cd geth
+    tar -xjf ../geth.tar.bz2
+fi
