2479ea8649d220d78af17520376de8ecaf028b10
Mikko Ohtamaa <mikko@opensourcehacker.com>
2016-09-09 17:12:49 +0300

    More test fixes and diagnostics

--- websauna/wallet/ethereum/geth.py
+++ websauna/wallet/ethereum/geth.py
@@ -11,7 +11,7 @@ from populus.utils.filesystem import get_blockchains_dir
 logger = logging.getLogger(__name__)
 
 
-def start_private_geth(chain_name, project_dir, host, port, verbosity=2, p2p_port=30303):
+def start_private_geth(chain_name, project_dir, host, port, verbosity=5, p2p_port=30303):
     """Start a local geth process that mines isolated private testnet."""
     blockchains_dir = get_blockchains_dir(project_dir)
 
