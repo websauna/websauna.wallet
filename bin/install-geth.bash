9096bf7475c09360f1e5ecfdf39d093524b4f913
Mikko Ohtamaa <mikko@opensourcehacker.com>
2017-02-27 14:43:44 +0200

    Try to figure geth installation on Travis.

--- bin/install-geth.bash
+++ bin/install-geth.bash
@@ -11,7 +11,13 @@ if [ ! -e geth/geth ] ; then
     install -d geth
     cd geth
     tar -xzf ../geth.tar.gz
+    mv geth-linux-amd64-1.5.9-a07539fb/geth .
     echo "Geth installed at $PWD/geth"
+    cd ..
 else
     echo "Geth already installed at $PWD/geth/geth"
+fi
+
+if [ ! -e geth/geth ] ; then
+    echo "geth binary not found in the extacted folder"
 fi
